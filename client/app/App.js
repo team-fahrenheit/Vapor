fef4e6d0c801b66a1fec672772210bdef7e15ae1
jason <potvinj@gmail.com>
2023-01-11 16:40:08 -0500

    add footer

--- client/app/App.js
+++ client/app/App.js
@@ -4,7 +4,6 @@ import AppRoutes from "./AppRoutes";
 import theme from "../features/MUI-Theme/MuiTheme";
 import { ThemeProvider } from "@mui/material/styles";
 import AllProducts from "../features/allProducts/allProducts";
-
 import Footer from "../features/footer/Footer";
 
 const App = () => {
@@ -15,6 +14,7 @@ const App = () => {
           <Navbar />
           <AppRoutes />
           <AllProducts />
+          <Footer />
         </div>
       </ThemeProvider>
     </React.StrictMode>
