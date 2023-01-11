a426425837a97c4d9d036902a0d175938c5a3ec6
jason <potvinj@gmail.com>
2023-01-11 16:16:50 -0500

    import footer to app

--- client/app/App.js
+++ client/app/App.js
@@ -3,7 +3,8 @@ import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
 import theme from "../features/MUI-Theme/MuiTheme";
 import { ThemeProvider } from "@mui/material/styles";
-import AllProducts from '../features/allProducts/allProducts';
+import AllProducts from "../features/allProducts/allProducts";
+import Footer from "../features/footer/Footer";
 
 const App = () => {
   return (
@@ -11,11 +12,10 @@ const App = () => {
       <div>
         <Navbar />
         {/* <AppRoutes /> */}
-        <AllProducts/>
+        <AllProducts />
         <Footer />
       </div>
     </ThemeProvider>
-
   );
 };
 
