1fa4b2dd64c75adbda47153a600c157401552033
jason <potvinj@gmail.com>
2023-01-11 12:02:54 -0500

    add footer

--- client/app/App.js
+++ client/app/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from '../features/navbar/Navbar';
-import AppRoutes from './AppRoutes';
+import Navbar from "../features/navbar/Navbar";
+import AppRoutes from "./AppRoutes";
+import Footer from "../features/footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <Footer />
     </div>
   );
 };
