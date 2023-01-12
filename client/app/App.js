cf819cddc97657f721b9edfacbec90559e9f7a33
jason <potvinj@gmail.com>
2023-01-12 11:29:25 -0500

    housekeeping

--- client/app/App.js
+++ client/app/App.js
@@ -3,7 +3,6 @@ import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
 import theme from "../features/MUI-Theme/MuiTheme";
 import { ThemeProvider } from "@mui/material/styles";
-import AllProducts from "../features/allProducts/allProducts";
 import Footer from "../features/footer/Footer";
 
 const App = () => {
