7ead90855ad3701b4527332f6a1fc0f7450d118e
jason <potvinj@gmail.com>
2023-01-13 10:58:14 -0500

    move card to own component

--- client/features/allProducts/allProducts.js
+++ client/features/allProducts/allProducts.js
@@ -1,23 +1,10 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectProducts, fetchAllProducts } from "./allProductsSlice";
-import {
-  Box,
-  Card,
-  CardMedia,
-  CardContent,
-  IconButton,
-  Typography,
-  Pagination,
-  Grid,
-  PaginationItem,
-} from "@mui/material";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Pagination, Grid, PaginationItem } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { getSearch } from "../navbar/SearchBarSlice";
-
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import RecipeReviewCard from "./cards";
 
 const AllProducts = () => {
@@ -51,7 +38,6 @@ const AllProducts = () => {
   };
 
   if (allProducts.products) {
-    //console.log(allProducts);
     return (
       <div>
         <Grid
@@ -65,52 +51,8 @@ const AllProducts = () => {
           {allProducts.products.map((product) => (
             <RecipeReviewCard product={product} />
           ))}
-          {/* <Grid xs item key={product.sku}>
-               <Card
-                variant="outlined"
-                sx={{
-                  width: 190,
-                  height: 270,
-                  backgroundColor: "background.paper",
-                  borderColor: "secondary.main",
-                  boxShadow: 2,
-                }}
-              >
-                <CardMedia
-                  sx={{
-                    width: 150,
-                    height: 150,
-                    margin: "auto",
-                    marginTop: 1,
-                    borderRadius: 3,
-                  }}
-                  image={product.image}
-                  title={product.name}
-                />
-                <CardContent>
-                  <Typography variant="body2" sx={{ textAlign: "center" }}>
-                    {product.albumTitle}
-                  </Typography>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Typography variant="body2" color="##6b6b6b">
-                      {product.regularPrice}
-                    </Typography>
-                    <IconButton aria-label="shopping-cart-button">
-                      <ShoppingCartIcon style={{ color: "#ff6700" }} />
-                    </IconButton>
-                    <button onClick={console.log(product)}>expand</button>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))} */}
         </Grid>
+
         <Pagination
           count={allProducts.totalPages}
           onChange={handleClickPageNumber}
